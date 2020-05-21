a2b29727adedb4e96e450236f552b96de096c1b5
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-05-21 17:30:10 -0600

    finish

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,5 +1,15 @@
 const skillsDB = require('../models/skill');
 
+module.exports = {
+    index,
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
+}
+
 function index(req, res, next) {
     res.render('skills/index', {
         skills: skillsDB.getAll()
@@ -12,7 +22,27 @@ function show(req, res, next) {
     })
 }
 
-module.exports = {
-    index,
-    show
+function newSkill(req, res) {
+    res.render('skills/new.ejs');
+}
+
+function create(req, res) {
+    skillsDB.create(req.body);
+    res.redirect('/skills');
+}
+
+function deleteSkill(req, res) {
+    skillsDB.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function edit(req, res) {
+    const skill = skillsDB.getOne(req.params.id);
+    res.render('skills/edit', {skill});
+}
+
+function update(req, res) {
+    req.body.expert = !!req.body.expert;
+    skillsDB.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
 }
--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,19 @@
 const skills = [
-    {id: 125223, skill: 'CSS'},
-    {id: 127904, skill: 'JavaScript'},
-    {id: 139608, skill: 'HTML'},
-    {id: 082212, skill: 'node'},
-    {id: 870501, skill: 'Python'}
+    {id: 125223, skill: 'CSS', expert: false},
+    {id: 127904, skill: 'JavaScript', expert: false},
+    {id: 286468, skill: 'Dancing', expert: true},
+    {id: 139608, skill: 'HTML', expert: false},
+    {id: 632686, skill: 'Special FX Makeup', expert: true},
+    {id: 082212, skill: 'Node', expert: false},
+    {id: 870501, skill: 'Python', expert: false},
 ];
 
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 };
 
 function getAll() {
@@ -17,4 +22,20 @@ function getAll() {
 
 function getOne(id) {
     return skills.find(skill => skill.id === parseInt(id));
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.expert = false;
+    skills.push(skill);
+}
+
+function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function update(id, skill) {
+    const skillObj = skills.find(skill => skill.id === parseInt(id));
+    Object.assign(skillObj, skill);
 }
